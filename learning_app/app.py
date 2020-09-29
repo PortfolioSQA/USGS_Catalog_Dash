1fe33f399024bbf0a2705fbd8f2dadaad561e09f
PortfolioSQA <59346324+PortfolioSQA@users.noreply.github.com>
2020-09-28 21:05:31 -0700

    Add files via upload

--- learning_app/app.py
+++ learning_app/app.py
@@ -17,18 +17,19 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 from dash.dependencies import Input, Output
+from etl import create_dfs
 
 # import plotly.express as px
 import plotly.graph_objs as go
 # from plotly.graph_objs import *
 
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-sample_data = pd.read_csv('sample_data.csv')
-sample_data2 = sample_data[['name', 'date', 'mission_area','science_topic', 'lat_n', 'lon_w']]
+#load data
+sample_data = pd.read_csv('sdc_sample.csv')
+df_map, df_US, df_earth = create_dfs(sample_data)
 
     
 # mapbox://styles/sdc-dash/ckf36pfn30e2119nyjf4nfycy
@@ -76,13 +77,12 @@ layout_map = dict(
         accesstoken=mapbox_access_token,
         # style="light",
         center=dict(
-            lon=-80,
-            lat=35.7342
+            lon=-95,
+            lat=39.7342
         ),
-        zoom=3,
+        zoom=1.5,
     )
 )
-
 
 app.layout = html.Div(
     html.Div([
@@ -98,79 +98,89 @@ app.layout = html.Div(
             [
                 html.Div(
                     [
-                        html.P('Select Mission Area(s):'),
-                        dcc.Checklist(
-                                id = 'miss_area',
-                                options=[
-                                    {'label': 'Core Science Systems', 'value': 'Core Science Systems'},
-                                    {'label': 'Ecosystems', 'value': 'Ecosystems'},
-                                    {'label': 'Energy and Minerals', 'value': 'Energy and Minerals'},
-                                    {'label': 'Environmental Health', 'value': 'Environmental Health'},
-                                    {'label': 'Land Resources', 'value': 'Land Resources'},
-                                    {'label': 'Natural Hazards', 'value': 'Natural Hazards'},
-                                    {'label': 'Regional Offices', 'value': 'Regional Offices'},
-                                    {'label': 'Water Resources', 'value': 'Water Resources'}
-                                ],
-                                value = [
-                                    'Core Science Systems',
-                                    'Ecosystems',
-                                    'Energy and Minerals',
-                                    'Environmental Health',
-                                    'Land Resources',
-                                    'Natural Hazards',
-                                    'Regional Offices',
-                                    'Water Resources'],
-                                labelStyle={'display': 'inline-block'}
-                        ),
+                        html.P('Select Dates:'),
+                        dcc.Slider(
+                            id = 'dates',
+                            min=1790,
+                            max=2020,
+                            step=None,
+                            marks={
+                                1790: 'Pre-1800',
+                                1900: '1900',
+                                1920: '1920',
+                                1940: '1940',
+                                1960: '1960',
+                                1980: '1980',  
+                                2000: '2000',
+                                2020: 'present'
+                            },
+                            value = 2020
+                        )  
+                        # dcc.Checklist(
+                        #         id = 'dates',
+                        #         options=[
+                        #             {'label': '2000', 'value': '2000'},
+                        #             {'label': '2001', 'value': '2001'},
+                        #             {'label': '2002', 'value': '2002'},
+                        #             {'label': '2003', 'value': '2003'},
+                        #             {'label': '2004', 'value': '2004'},
+                        #             {'label': '2005', 'value': '2005'},
+                        #             {'label': '2006', 'value': '2006'},
+                        #             {'label': '2007', 'value': '2007'},
+                        #         ],
+                        #         value = ['2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', 'nan'],
+                        #         labelStyle={'display': 'inline-block'}
+                        # ),
                     ],
                     className='six columns',
                     style={'margin-top': '10'}
                     ),
                 html.Div(
                     [
-                        html.P('Select Science Topic(s) from dropdown menu:'),
+                        html.P('Select Science Center:'),
                         dcc.Dropdown(
                             id='sci_topic',
                             options= [{'label': str(item),'value': str(item)}
-                                      for item in set(sample_data['science_topic'])],
+                                      for item in set(df_map['sci_center'])],
                             # multi=True,
-                            value= "Energy Resources")
+                            value= "Fort Collins Science Center")
                     ],
                     className='six columns',
                     style={'margin-top': '10'}
                 )],
             className='row' ),
-     # Map + table + Histogram
+      # Map + table + Histogram
         html.Div(
             [
                 html.Div(
                     [
                         dcc.Graph(id='map-graph',
-                                  animate=True,
                                   style={'margin-top': '20'})
                     ], className = "six columns"
                 ),
                 html.Div(
                     [
                         dash_table.DataTable(id='datatable',
-                                columns=[{"name": i, "id": i} for i in sample_data2.columns],
-                                data=sample_data2.to_dict('records'),
+                                columns=[{"name": i, "id": i} for i in ["sci_center", "kw"]],
+                                style_cell={
+                                    'whiteSpace': 'normal',
+                                    'height': 'auto',
+                                },
+                                data=df_map.to_dict('records'),
                                 editable=True,
                                 sort_action="native",
                                 sort_mode="multi",
-                                row_selectable="multi",
-                                selected_rows=[],
+                                # row_selectable="multi",
+                                # selected_rows=[],
                                 page_action="native",
                                 page_current= 0,
                                 page_size= 10,
-                                style_table={
-                                    'overflowY': 'scroll',
-                                    })
+                                )
                     ], className="six columns"
                 ),
                 html.Div([
                         dcc.Graph(
-                            id='bar-graph'
+                            id='agg-graph'
                         )
                     ], className= 'twelve columns'
                     ),
@@ -180,41 +190,27 @@ app.layout = html.Div(
 
 @app.callback(
     Output('datatable', 'data'),
-    [Input('miss_area', 'value'),
-     Input('sci_topic', 'value')])
-def table_selection(miss_area, sci_topic):
-    df_ms = sample_data2.copy()
-    df_ms = df_ms[df_ms['mission_area'].isin(miss_area)]
-    df_ms = df_ms.loc[df_ms['science_topic'] == sci_topic]
+    [Input('dates', 'value'),
+      Input('sci_topic', 'value')])
+def table_selection(dates, sci_center):
+    # if len(dates) == 0:
+    #     return no_update
+    if len(sci_center) == 0:
+        return no_update
+    df_ms = df_map.copy()
+    df_ms = df_ms[df_ms['beg_year']< int(dates)]
+    df_ms = df_ms.loc[df_ms['sci_center'] == sci_center]
     return df_ms.to_dict("records")
-def update_selected_row_indices(miss_area, sci_topic):
-    map_aux = sample_data.copy()
-    map_aux = map_aux[map_aux['mission_area'].isin(miss_area)]
-    map_aux = map_aux.loc[map_aux["science_topics"] == sci_topic]
+def update_selected_row_indices(dates, sci_center):
+    # if len(dates) == 0:
+    #     return no_update
+    if len(sci_center) == 0:
+        return no_update
+    map_aux = df_map.copy()
+    map_aux = map_aux[map_aux['beg_year'] < int(dates)]
+    map_aux = map_aux.loc[map_aux["sci_center"] == sci_center]
     rows = map_aux.to_dict('records')
     return rows
-
-def gen_map(d):
-    # groupby returns a dictionary mapping the values of the first field
-    # 'classification' onto a list of record dictionaries with that
-    # classification value.
-    return {
-        "data": [{
-                "type": "scattermapbox",
-                "lat": list(d['lat_n']),
-                "lon": list(d['lon_w']),
-                "hoverinfo": "text",
-                "hovertext": [["{} <br>{} <br>{}".format(i,j,k)]
-                                for i,j,k in zip(d['name'], d['mission_area'],d['science_topic'])],
-                "mode": "markers",
-                "name": list(d['name']),
-                "marker": {
-                    "size": 6,
-                    "opacity": 0.7
-                }
-        }],
-        "layout": layout_map
-    }
 
 @app.callback(
     Output('map-graph', 'figure'),
@@ -223,37 +219,57 @@ def map_selection(data):
     aux = pd.DataFrame(data)
     if len(data) == 0:
         return no_update
-    return gen_map(aux)
+    else:
+        return {
+            "data": [{
+                    "type": "scattermapbox",
+                    "lat": list(aux['lat']),
+                    "lon": list(aux['lon']),
+                    "hoverinfo": "text",
+                    "hovertext": [["{}".format(i)]
+                                    for i in aux['sci_center']],
+                    "mode": "markers+text",
+                    "name": list(aux['sci_center']),
+                    "marker": {
+                        "size": 8,
+                        "opacity": 0.7},
+            }],
+            "layout": layout_map
+        }
 
-# @app.callback(
-#     Output('bar-graph', 'figure'),
-#     [Input('datatable', 'data')])
-# def update_figure(data):
-#     dff = pd.DataFrame(data)
-#     layout = go.Layout(
-#         bargap=0.05,
-#         bargroupgap=0,
-#         barmode='group',
-#         showlegend=False,
-#         dragmode="select",
-#         xaxis=dict(
-#             showgrid=False,
-#             nticks=50,
-#             fixedrange=False
-#         ),
-#         yaxis=dict(
-#             showticklabels=True,
-#             showgrid=False,
-#             fixedrange=False,
-#             rangemode='nonnegative',
-#             zeroline='hidden'
-#         )
-#     )
-#     data = go.Bar(
-#               x=dff.groupby('mission_area', as_index = False).count()['mission_area'],
-#               y=dff.groupby('mission_area', as_index = False).count()['science_topic']
-#           )
-#     return go.Figure(data=data, layout=layout)
+
+
+@app.callback(
+    Output('agg-graph', 'figure'),
+    [Input('datatable', 'data')])
+def update_figure(data):
+    if len(data) == 0:
+        return no_update
+    dff = pd.DataFrame(data)
+    layout = go.Layout(
+        bargap=0.05,
+        bargroupgap=0,
+        barmode='group',
+        showlegend=False,
+        dragmode="select",
+        xaxis=dict(
+            showgrid=False,
+            nticks=50,
+            fixedrange=False
+        ),
+        yaxis=dict(
+            showticklabels=True,
+            showgrid=False,
+            fixedrange=False,
+            rangemode='nonnegative',
+            zeroline= False 
+        )
+    )
+    data = go.Bar(
+              x=dff['sci_center'],
+              y=dff.groupby('sci_center', as_index = False).count()["sci_center"]
+          )
+    return go.Figure(data=data, layout=layout)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
